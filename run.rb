19b87cac39eb4c26761b0252275d495da2e7c38c
Andre Chalom <andrechalom@gmail.com>
2016-03-15 01:19:08 -0300

    Correção do issue #53

--- run.rb
+++ run.rb
@@ -1,4 +1,4 @@
-#!/home/marciomr/.rvm/rubies/ruby-2.2.1/bin/ruby
+#!/usr/bin/env ruby
 # coding: utf-8
 
 require 'koala'
@@ -24,19 +24,25 @@ end
 def populate_posts(page, id)
   posts = connect(id, 'posts')
   total(posts) do |post|
+    # A linha abaixo encerra o laço caso o post esteja fora do limite de tempo
+    break if (Time.parse(post['created_time']) < LIMIT)
+    # A linha abaixo pula esse post caso já exista no banco de dados:
     next if !Post.where(fb_id: post['id']).empty?
     shares_count = post['shares']['count'] if post['shares']
 
-    new_post = page.posts.create fb_id: post['id'],
-                                 message: post['message'],
-                                 cathegory: post['type'],
-                                 shares_count: shares_count
-    max = @graph.get_object(post['id'], fields: "likes.summary(true)")['likes']['summary']['total_count']
-    puts Time.parse(post['created_time']).strftime('%d/%m/%Y %H:%M:%S')
-    @bar = ProgressBar.new(max)
-    break if (Time.parse(post['created_time']) < LIMIT)
-    new_post.likes_count = populate_likes(page, new_post, post['id'])
-    new_post.save
+    # Englobamos o download em uma transação - assim se houver um erro
+    # no meio do download, o post não fica salvo pela metade
+    ActiveRecord::Base.transaction do
+        new_post = page.posts.create fb_id: post['id'],
+                                     message: post['message'],
+                                     cathegory: post['type'],
+                                     shares_count: shares_count
+        max = @graph.get_object(post['id'], fields: "likes.summary(true)")['likes']['summary']['total_count']
+        puts Time.parse(post['created_time']).strftime('%d/%m/%Y %H:%M:%S')
+        @bar = ProgressBar.new(max)
+        new_post.likes_count = populate_likes(page, new_post, post['id'])
+        new_post.save
+    end
   end
 end
 
@@ -52,6 +58,7 @@ else
                        likes_count: page['likes'],
                        name: page['name']
 end
+
 puts "Baixando posts de #{page['name']}..."
 puts "Serão processados todos os posts até dia #{LIMIT.strftime('%d/%m/%Y')}."
 puts "Isso pode levar várias horas."
@@ -59,7 +66,7 @@ puts "Isso pode levar várias horas."
 new_page.posts_count = populate_posts(new_page, $id)
 new_page.save
 
-puts "Calucalando quantos a intersecção dos conjuntos..."
+puts "Calculando a intersecção dos conjuntos..."
 count = (Set.new(Confirmed.all.map(&:user)) & new_page.likes_total).size
 
 puts "#{page['name']}"
