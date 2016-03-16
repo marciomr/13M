cbed30d725bb936e5f2ab8a620af8036cfeeaba0
Andre Chalom <andrechalom@gmail.com>
2016-03-15 21:34:47 -0300

    Remocao de funcao espuria

--- connect_fb.rb
+++ connect_fb.rb
@@ -61,9 +61,6 @@ class FB
     def total_attending(id)
         return @graph.get_object(id, fields: "attending.summary(true)")['attending']['summary']['count']
     end
-    def graph
-        return @graph
-    end
     # entra na pagina, loga no FB e devolve o código
     def get_access_code(page)
         @agent = Mechanize.new
