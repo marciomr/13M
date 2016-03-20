496f235d3b31e22db8c22f2887f8b60585bb43c4
Marcio M. Ribeiro <marcio.mr@gmail.com>
2016-03-20 20:25:57 -0300

    o mais rapido que conisgo deixar o results

--- results.rb
+++ results.rb
@@ -7,25 +7,17 @@ IDs = YAML.load_file("IDs.yaml")
 
 IDs["events"].each do |event, ids|
   puts "Resultados #{event}"; puts "Lendo banco de dados..."
-  events = Event.where(fb_id: ids)
-  confirmeds = Set.new
-  events.all.each do |e|
-    confirmeds.merge(e.confirmations.map(&:user_id))
-  end
-  # acho que dá pra fazer isso de maneira mais eficiente
-#confirmeds = Event.interacting
 
-
-  Page.all.each { |p| 
+  Page.all.each do |p| 
     i = p.interacting
     l = p.likes_total
     puts "Página #{p.name}"
-    puts "Interagiram no período: #{i.size}; likes: #{l}; média: #{ (l.to_f / i.size).round(2) } likes por usuário"
-    count = Set.new( i & confirmeds ).size
+    puts "Interagiram no período: #{i.count}; likes: #{l}; média: #{ (l.to_f / i.count).round(2) } likes por usuário"
+    count = i.join("JOIN confirmations").where(events_id: ids).count
     puts "Confirmados em #{event}: #{count}"
 
     CSV.open("#{event}.csv", "a+") do |csv|
-      csv << [p.name, i.size, l, count]
+      csv << [p.name, i.count, l, count]
     end
-  }
+  end
 end
